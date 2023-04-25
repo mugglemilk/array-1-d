80aab2c33e9135c60770df1d80138463a59e19fc
PUMIWATR_K <120041384+mugglemilk@users.noreply.github.com>
2023-04-25 14:27:29 +0700

    Add files via upload

---
+++ Box2.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+
+public class Box2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] array = new int[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            array[i] = scan.nextInt();
+        }
+        int k = scan.nextInt();
+        int[] sum = new int[n + 1];//ของที่มี
+        for (int i = 1; i <= k; ++i) {
+            int x = scan.nextInt();
+
+            if (x == 1) {
+                int y = scan.nextInt();
+                int z = scan.nextInt();
+                if (array[z] >= sum[z] + y) {
+                    sum[z] += y;
+                    System.out.println(sum[z]);
+                } else {
+                    System.out.println(-1);
+                }
+            } else if (x == 2) {
+                int y = scan.nextInt();
+                for (int j = 1; j <= n; ++j) {
+                    if (sum[j] >= y) {
+                        sum[j] -= y;
+                        System.out.println(j + " " + sum[j]);
+                        break;
+                    } else {
+                        y -= sum[j];
+                        sum[j] = 0;
+                       // System.out.print("j " + j + " y: " + y);
+                    }
+                    if (j == n) {
+                        System.out.println(j + " 0");
+                    }
+                }
+
+            }
+        }
+    }
+}
